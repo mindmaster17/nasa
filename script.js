4987c03325efb1c94824e1a29fa8838b648516f9
mindmaster17 <127671399+mindmaster17@users.noreply.github.com>
2024-10-01 18:32:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Adding a click event to the "Download Now" button
+const downloadButton = document.querySelector('.button');
+
+downloadButton.addEventListener('click', () => {
+    // Add your download logic here (e.g., redirect to app store)
+    console.log('Download button clicked');
+});
